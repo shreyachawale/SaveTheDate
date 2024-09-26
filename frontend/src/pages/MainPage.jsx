76998ce9293278efb77c49f56c0c158096a8a773
Jorden <jordenmathew55@gmail.com>
2024-09-26 23:51:35 +0530

    added UserDashboard

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 // HomePage.js
 import Carousel from '../components/Carousal'
 import Header from '../components/Header'
+import UserDashboard from './HostDashboard'
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <Header />
       <main className="flex-grow">
         <Carousel />
+        <UserDashboard/>
       </main>
       <footer className="bg-gray-100 py-8">
         <div className="container mx-auto px-4 text-center text-gray-600">
